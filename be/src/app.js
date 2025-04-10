c700229fff810a1f34e517b44c7e5a69ed5dc383
levanhuyhoang <lvhhoangg171@gmail.com>
2025-04-11 00:49:13 +0700

    swagger

--- be/src/app.js
+++ be/src/app.js
@@ -6,7 +6,7 @@ const helmet = require("helmet");
 const compression = require("compression");
 const os = require("os");
 const router = require("./routes/index");
-
+const { swaggerUi, swaggerSetup } = require("./configs/swagger.config");
 // cpu thread number : luồng xử lý
 // process.env.UV_THREADPOOL_SIZE = os.cpus().length;
 
@@ -27,8 +27,12 @@ require("./dbs/init.mongodb");
 // init routes
 app.use("/v1/api", router);
 
+// swagger
+app.use("/api-docs", swaggerUi.serve);
+app.get("/api-docs", (req, res) => {
+  res.send(swaggerSetup(req, res));
+});
 // handling errors
-
 app.use((req, res, next) => {
   const error = new Error("Not found");
   error.status = 404;
